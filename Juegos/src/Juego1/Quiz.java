bb4e2d7a987aa9a33d88aba0ad2c3b28c4424e63
RakuKinG <63345441+RakuKinG@users.noreply.github.com>
2020-04-10 23:46:06 +0200

    Update Quiz.java

--- Juegos/src/Juego1/Quiz.java
+++ Juegos/src/Juego1/Quiz.java
@@ -1,8 +1,27 @@
 package Juego1;
+import java.util.Scanner;
 
 public class Quiz {
-	System.out.println("¿De que año es bob esponja?");
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("¿En que siglo se inventó la catapulta?");
+		
+		System.out.println("100 d de C (1)");
+		System.out.println("500 a de C (2)");
+		System.out.println("400 a de C (3)");
+		System.out.println("197 d de C (4)");
+		
+		String opcion = sc.nextint();
+		sc.nextLine();
+		
+		if(opcion==3){
+			System.out.println("Correcto!");
+		}else{
+			System.out.println("Incorrecto!");
+		}
+		System.out.println("FIN");
 	
-	
+	}
 	
 }
